b69d6d745104f86d46245d7b322876d3a6fcaf3b
H4zlq <haziqnaim72@gmail.com>
2021-12-27 08:00:12 +0800

    Add `context menu` collection and register context menu handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { Client, Collection } = require('discord.js');
-const { registerCommands, registerSlashCommands, registerEvents } = require('./utils/registry');
-const { token, prefix } = require('../config.json');
+const { registerCommands, registerSlashCommands, registerEvents, registerContextMenus } = require('./utils/registry');
+const { token, prefix, useDatabase } = require('../config.json');
 
 class MyClient extends Client {
     constructor({ token }) {
@@ -9,6 +9,7 @@ class MyClient extends Client {
         // Collection
         this.commands = new Collection();
         this.slashCommands = new Collection();
+        this.contextMenus = new Collection();
         this.events = new Collection();
         this.token = token;
         this.prefix = prefix;
@@ -16,13 +17,19 @@ class MyClient extends Client {
         // Register handler
         this.registerHandlers();
 
-        // Connect to Mongo Database
-        require('./database/mongo/connection');
+        /**
+         * Connect to Mongo Database
+         * if you dont want to use database
+         * please set `useDatabase` to false
+         * on config.
+         */
+        if (useDatabase) require('./database/mongo/connection'); 
     }
 
     async registerHandlers() {
         await registerCommands(this, '../commands');
-        await registerSlashCommands(this, '../slashCommands')
+        await registerSlashCommands(this, '../slashCommands');
+        await registerContextMenus(this, '../contextMenus');
         await registerEvents(this, '../events');
     }
 
